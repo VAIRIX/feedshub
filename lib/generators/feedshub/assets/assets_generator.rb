e749737916fad0a82f176c80a2002134dba60084
Daniel Fernández <dfernandez@vairix.com>
2014-03-20 16:36:45 -0300

    generator of assets in parent application was added Cr: MV

---
+++ lib/generators/feedshub/assets/assets_generator.rb
@@ -0,0 +1,33 @@
+require 'rails/generators'
+
+module Feedshub
+  module Generators
+    class AssetsGenerator < ::Rails::Generators::Base
+
+      source_root File.expand_path('../templates', __FILE__)
+      desc 'This generator installs Feedshub to Asset Pipeline'
+
+      def add_assets
+        css_manifest = 'app/assets/stylesheets/application.css'
+
+        if File.exist?(css_manifest)
+          # Add our own require:
+          content = File.read(css_manifest)
+          if content.match(/require_tree\s+\.\s*$/)
+            # Good enough - that'll include our bootstrap_and_overrides.css.less
+          else
+            style_require_block = " *= require feedshub/feeds\n"
+            insert_into_file css_manifest, style_require_block, :after => "require_self\n"
+          end
+        else
+          copy_file 'application.css', 'app/assets/stylesheets/application.css'
+        end
+      end
+
+      def add_styles
+        copy_file 'feeds.css', 'app/assets/stylesheets/feedshub/feeds.css'
+      end
+
+    end
+  end
+end
