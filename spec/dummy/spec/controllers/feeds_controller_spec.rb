695a6d66c09e4271cc887e0fe44252969043277c
Martin Villero <mvillero@vairix.com>
2014-03-21 09:51:39 -0300

    Add context to avoid unnecessary renders; CR: EF

--- spec/dummy/spec/controllers/feeds_controller_spec.rb
+++ spec/dummy/spec/controllers/feeds_controller_spec.rb
@@ -4,32 +4,34 @@ describe Feedshub::FeedsController do
   routes { Feedshub::Engine.routes }
 
   describe 'github_feeds' do
-    render_views
 
     before :each do
       links =["File://#{Rails.root}/spec/resources/member.atom"]
       GitFeedsHandler.fetch_and_create_feeds_enrties(links)
     end
 
-    it 'response ok' do
-      get :github_feeds
-      expect(response).to be_succes
+    context 'check content correctness' do
+      render_views
+
+      it 'has to render partial github_feeds' do
+        get :github_feeds
+        expect(response.body).to match /Recent Feeds/
+      end
+      it 'has to render partial github_feeds with atom entries' do
+        get :github_feeds
+        expect(response.body).to match /vairix-dfernandez/
+      end
     end
 
-    it 'has to render partial github_feeds' do
-      get :github_feeds
-      expect(response.body).to match /Recent Feeds/m
+    context 'check status' do
+      it 'response ok' do
+        get :github_feeds
+        expect(response).to be_succes
+      end
+      it 'has to assign @entries' do
+        get :github_feeds
+        expect(assigns(:entries).size).to eq(Feedshub::GithubFeedEntry::LAST_ADDED)
+      end
     end
-
-    it 'has to assign @entries' do
-      get :github_feeds
-      expect(assigns(:entries).size).to eq(Feedshub::GithubFeedEntry::LAST_ADDED)
-    end
-
-    it 'has to render partial github_feeds with atom entries' do
-      get :github_feeds
-      expect(response.body).to match /vairix-dfernandez/m
-    end
-
   end
 end
