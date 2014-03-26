03fcc8be06e9a592aad0c2d8cd33538963851d02
Martin Villero <mvillero@vairix.com>
2014-03-26 11:29:27 -0300

    Refactor of model scope and helper; CR GA

--- app/models/feedshub/github_feed_entry.rb
+++ app/models/feedshub/github_feed_entry.rb
@@ -1,7 +1,7 @@
 module Feedshub
   class GithubFeedEntry < ActiveRecord::Base
 
-    scope :public_feeds, -> (limit = nil) { order(published: :desc).limit(limit.blank? ? ENV['github_feeds_number'].to_i : limit) }
+    scope :public_feeds, -> (limit = ENV['github_feeds_number'].to_i) { order(published: :desc).limit(limit) }
 
   end
 end
