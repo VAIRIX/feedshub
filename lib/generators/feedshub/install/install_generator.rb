bd41bfc2194cd92b33642f65271f6ee2f7202a96
Martin Villero <mvillero@vairix.com>
2014-04-01 17:02:19 -0300

    Fix configuration file in order to avoid duplicate entries + responsive support; CR:DF

--- lib/generators/feedshub/install/install_generator.rb
+++ lib/generators/feedshub/install/install_generator.rb
@@ -12,9 +12,9 @@ module Feedshub
 
         if File.exist?(css_manifest)
           content = File.read(css_manifest)
-          unless content.match(/require_tree\s+\.\s*$/)
-            style_require_block = ' *= require feedshub/feeds\n'
-            insert_into_file css_manifest, style_require_block, :after => 'require_self\n'
+          unless content.match(/require_tree\s+\.\s*$/) || content.match(/require feedshub\/feeds/)
+            style_require_block = "\r\n*= require feedshub/feeds\r\n"
+            insert_into_file css_manifest, style_require_block, :before => "*/"
           end
         else
           copy_file 'application.css', 'app/assets/stylesheets/application.css'
@@ -23,8 +23,8 @@ module Feedshub
         js_manifest = 'app/assets/javascripts/application.js'
         if File.exist?(js_manifest)
           content = File.read(js_manifest)
-          unless content.match(/require_tree\s+\.\s*$/)
-            js_require_block = ' //= require feedshub/feeds\n'
+          unless content.match(/require_tree\s+\.\s*$/) || content.match(/require feedshub\/feeds/)
+            js_require_block = "\r\n//= require feedshub/feeds"
             append_to_file js_manifest, js_require_block
           end
         else
@@ -41,10 +41,12 @@ module Feedshub
       end
 
       def add_config
-        conf_manifest = 'config/feedshub/config.yml'
-        env_update = "ENV.update YAML.load(File.read(File.expand_path('../feedshub/config.yml', __FILE__)))\n"
-        copy_file 'config.yml', conf_manifest
-        append_to_file 'config/application.rb', env_update
+        unless File.read('config/application.rb').match(/feedshub\/config.yml/)
+          conf_manifest = 'config/feedshub/config.yml'
+          env_update = "\r\nENV.update YAML.load(File.read(File.expand_path('../feedshub/config.yml', __FILE__)))\n"
+          copy_file 'config.yml', conf_manifest
+          append_to_file 'config/application.rb', env_update
+        end
       end
 
       def copy_schedule
